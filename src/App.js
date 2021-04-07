88b55ba9a7a8afc7c8483624231be9b62b5cff19
Emeka Nwadiogbu, Jr <emekanwadiogbu.jr@me.com>
2021-04-06 22:01:01 -0700

    Emotion buttons

--- src/App.js
+++ src/App.js
@@ -1,53 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import restaurant from "./restaurant.jpg";
 
-function Header(props) {
-  return (
-    <header>
-      <h1>{props.name} Kitchen</h1>
-    </header>
-  );
-}
-
-function Main(props) {
-  return (
-    <section>
-      <p>We serve the most {props.adjective} food in town!</p>
-      <img src={restaurant} />
-      <ul style={{textAlign: 'left'}}>
-      {props.dishes.map((dish) => (
-        <li key={dish.id}>{dish.title}</li>
-      ))}
-      </ul>
-    </section>
-  );
-}
-
-function Footer(props) {
-  return (
-    <footer>
-      <p>Copyright {props.year}</p>
-    </footer>
-  );
-}
-
-const dishes = [
-  "Macaroni and Cheese",
-  "Salmon",
-  "Tofu with Vegetables",
-  "Minestrone"
-];
-
-const dishObjects = dishes.map((dish, i) => ({ id: i, title: dish }));
 
 function App() {
+  const [emotion, setEmotion] = useState("happy");
+
+  console.log(emotion);
   return (
-    <div className="App">
-      <Header name="Mama's" />
-      <Main adjective="amazing" dishes={dishObjects} />
-      <Footer year={new Date().getFullYear()} />
-    </div>
+    <>
+    <h1>Current emotion is {emotion}.</h1>
+    <button onClick = {() => setEmotion("happy")}>
+    Happy  
+    </button>
+    <button onClick = {() => setEmotion("frustrated")}>
+    Frustrate 
+    </button>
+    <button onClick={() => setEmotion("enthusiastic")}>Enthuse</button>
+    </>
   );
 }
 
